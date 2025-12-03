f32ccf96a97c940772d04e7532a1c65e354542c1
Oliver Metz <softwaredevdad@gmail.com>
2025-12-03 12:14:44 +0100

    Setup User mongoose schema. add some pre hook/methods and an index for the users username

---
+++ user-service/src/models/User.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(12);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+      return next(error);
+    }
+  }
+});
+
+userSchema.methods.comparePassword = async function (passwordToCompare) {
+  try {
+    return await bcrypt.compare(passwordToCompare, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.index({ username: "text" });
+
+export const User = mongoose.model("User", userSchema);
