29e500bb615aeb3881b24060b82fc7f2d533b59d
Oliver Metz <softwaredevdad@gmail.com>
2025-12-03 23:46:24 +0100

    update our api-entrypoint server to log our running services(currently 1)

--- api-entrypoint/src/server.js
+++ api-entrypoint/src/server.js
@@ -1,6 +1,11 @@
 import "dotenv/config";
 import { app } from "./app.js";
+import { logger } from "devdad-express-utils";
 
 app.listen(process.env.PORT || 3000, () => {
   logger.info(`API-Entrypoint is running on port ${process.env.PORT || 3000}`);
+  logger.info(
+    `User-Service is running on port ${process.env.USER_SERVICE_URL || 3001}`,
+  );
+  logger.info("Redis is running on ->", { redisUrl: process.env.REDIS_URL });
 });
