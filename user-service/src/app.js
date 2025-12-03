8c05d12303f082a69d77e20eea11e6329f26844e
Oliver Metz <softwaredevdad@gmail.com>
2025-12-03 14:34:50 +0100

    more setup in the server.js, use our logger to log out the dbstatus information. - app.js add some more compulsory middlewares(still more to add) - Add user router.

--- user-service/src/app.js
+++ user-service/src/app.js
@@ -1,16 +1,23 @@
 import express from "express";
+import "dotenv/config";
 import { errorHandler } from "devdad-express-utils";
+import userRouter from "./routes/user.routes.js";
+import helmet from "helmet";
 
 //#region Constants
 const app = express();
 //#endregion
 
 //#region Middleware
-
+app.use(helmet());
+//TODO: Add CORS custom configuration
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 //#endregion
 
 //#region Route Entry Points
-
+app.use("/api/v1/user", userRouter);
 //#endregion
 
 //#region Global Error Handler
--- user-service/src/server.js
+++ user-service/src/server.js
@@ -1,9 +1,11 @@
-import { connectDB, getDBStatus } from "devdad-express-utils";
-import { app } from "./app";
+import { connectDB, getDBStatus, logger } from "devdad-express-utils";
+import { app } from "./app.js";
 
 await connectDB();
 
+const dbStatus = getDBStatus();
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
-  console.log(getDBStatus());
+  logger.info("DB Status", dbStatus);
 });
