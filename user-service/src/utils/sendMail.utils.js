19ea6c8d28470c5c1300a73928e82093dead3762
Oliver Metz <softwaredevdad@gmail.com>
2026-01-04 13:20:33 +0100

    Update: refactor our nodemailer to use resend instead.
    Changed from nodemailer to resend as issues with railway blocking SMTP
    due to spam etc, need to be a pro tier, but at least it works on local
    host for development, converted to using resend which uses http requests
    and apis for sending emails, better and more reliable over time. hooked
    it up with a cheap custom domain and custom email for that domain and
    now it works flawlessly, with minimal refactoring.

--- user-service/src/utils/sendMail.utils.js
+++ user-service/src/utils/sendMail.utils.js
@@ -2,22 +2,32 @@ import { AppError, logger } from "devdad-express-utils";
 import "dotenv/config";
 import nodemailer from "nodemailer";
 import path from "path";
+import { Resend } from "resend";
 
 //TODO: add .env vars later
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
-  service: process.env.SMTP_SERVICE,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: Number(process.env.SMTP_PORT) || 587,
+//   service: process.env.SMTP_SERVICE,
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async (to, subject, html) => {
   try {
-    await transporter.sendMail({
-      from: process.env.SMTP_FROM,
+    // await transporter.sendMail({
+    //   from: process.env.SMTP_FROM,
+    //   to,
+    //   subject,
+    //   html,
+    // });
+
+    await resend.emails.send({
+      from: process.env.RESEND_FROM,
       to,
       subject,
       html,
